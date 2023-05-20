95a2ab52b14af2bc06636a6c6b9a70565ee8a496
Ignacio Gavia <iganaciogavia@gmail.com>
2023-05-20 16:07:35 -0400

    implementacion del repositorio

--- src/coleccion/ListaInstrumento.java
+++ src/coleccion/ListaInstrumento.java
@@ -7,5 +7,21 @@ public class ListaInstrumento {
     private int cantidadInstrumento;
     private Instrumento[] listaInstrumento;
 
+    public ListaInstrumento(int cantidadMaxima, int cantidadInstrumento, Instrumento[] listaInstrumento) {
+        this.cantidadMaxima = cantidadMaxima;
+        this.cantidadInstrumento = cantidadInstrumento;
+        this.listaInstrumento = new Instrumento[cantidadMaxima];
+    }
 
+    public int getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
+    public int getCantidadInstrumento() {
+        return cantidadInstrumento;
+    }
+
+    public Instrumento[] getListaInstrumento() {
+        return listaInstrumento;
+    }
 }
